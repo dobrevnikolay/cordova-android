39c2792eb8a53ad7dbb8dcc7e1105e2cfcdcae7b
ndobrev <ndobrevgw@gmail.com>
2021-12-26 18:12:02 +0100

    set allow content access

--- framework/src/org/apache/cordova/engine/SystemWebViewEngine.java
+++ framework/src/org/apache/cordova/engine/SystemWebViewEngine.java
@@ -159,7 +159,9 @@ public class SystemWebViewEngine implements CordovaWebViewEngine {
          * It is recommended to turn off this settings To prevent possible security issues targeting Build.VERSION_CODES.Q and earlier.
          * For existing functionality, this setting is set to true. In a future release, this should be defaulted to false.
          */
+
         settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
         settings.setAllowUniversalAccessFromFileURLs(true);
 
         String manufacturer = android.os.Build.MANUFACTURER;
